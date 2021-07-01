f7aca04e0238d68fd66aeea9a62376306c5d41ad
Kennedy Mutemi <44292630+Mutemi1998@users.noreply.github.com>
2021-07-01 20:52:06 +0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,7 +36,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'funiture.apps.FunitureConfig',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -47,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
