6d93b8c4159aab7b4239ab5a919af84835c049aa
Kenne <kenmuthui65@gmail.com>
2021-07-23 05:04:38 +0300

    Added files and changes required for deployment to heroku

--- funiture/urls.py
+++ funiture/urls.py
@@ -6,8 +6,28 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('product', views.product, name='product'),
     path('AllProduct', views.AllProduct, name='AllProduct'),
+
     path('AllHome', views.AllHome, name='AllHome'),
     path('dinnerware', views.dinnerware, name='dinnerware'),
     path('glassware', views.glassware, name='glassware'),
 
+    path('AllLiving', views.AllLiving, name='AllLiving'),
+    path('recliner', views.recliner, name='recliner'),
+    path('sofa', views.sofa, name='sofa'),
+    path('tv', views.tv, name='tv'),
+
+    path('Allbedroom', views.Allbedroom, name='Allbedroom'),
+    path('bed', views.recliner, name='bed'),
+    path('study', views.sofa, name='study'),
+    path('dressing', views.tv, name='dressing'),
+
+    path('AllOffice', views.AllOffice, name='AllOffice'),
+    path('confrence', views.confrence, name='confrence'),
+    path('executive', views.executive, name='executive'),
+    path('office', views.office, name='office'),
+
+    path('AllDinning', views.AllDinning, name='AllDinning'),
+    path('barChair', views.barChair, name='barChair'),
+    path('dinningChair', views.dinningChair, name='dinningChair'),
+    path('dinningSet', views.dinningSet, name='dinningSet'),
 ]
--- funiture/views.py
+++ funiture/views.py
@@ -15,6 +15,8 @@ def AllProduct(request):
 def product(request):
     return render(request, 'product.html')
 
+
+# functions for calling home links
 def dinnerware(request):
     return render(request, 'Home/dinnerware.html')
 
@@ -23,3 +25,61 @@ def glassware(request):
 
 def AllHome(request):
     return render(request, 'Home/AllProduct.html')
+
+
+# functions for calling living links
+def sofa(request):
+    return render(request, 'Living/sofa.html')
+
+def recliner(request):
+    return render(request, 'Living/recliner.html')
+
+def tv(request):
+    return render(request, 'Living/tv.html')
+
+def AllLiving(request):
+    return render(request, 'Living/AllLiving.html')
+
+
+# functions for calling Bedroom links
+def Bed(request):
+    return render(request, 'Bedroom/bed.html')
+
+def Study(request):
+    return render(request, 'Bedroom/study.html')
+
+def Dressing(request):
+    return render(request, 'Bedroom/dressing.html')
+
+def Allbedroom(request):
+    return render(request, 'Bedroom/Allbedroom.html')
+
+
+
+# functions for calling Office links
+def confrence(request):
+    return render(request, 'Office/confrence.html')
+
+def executive(request):
+    return render(request, 'Office/executive.html')
+
+def office(request):
+    return render(request, 'Office/office.html')
+
+def AllOffice(request):
+    return render(request, 'Office/Alloffice.html')
+
+
+
+# functions for calling Dinning links
+def barChair(request):
+    return render(request, 'Dinning/barChair.html')
+
+def dinningChair(request):
+    return render(request, 'Dinning/dinningChair.html')
+
+def dinningSet(request):
+    return render(request, 'Dinning/dinningSet.html')
+
+def AllDinning(request):
+    return render(request, 'Dinning/AllDinning.html')
